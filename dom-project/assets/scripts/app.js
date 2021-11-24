5c9c17ba9e84502411d27e0b90226e9f151dc7fd
enigma6174 <enigma6174@outlook.com>
2021-11-24 19:13:15 +0530

    Section 7: Development Complete; Bug Fix Complete

--- dom-project/assets/scripts/app.js
+++ dom-project/assets/scripts/app.js
@@ -5,65 +5,69 @@ const modalCancelButton = addMovieModal.querySelector(".btn--passive");
 const modalAddButton = modalCancelButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
 const entryTextSection = document.getElementById("entry-text");
+const deleteMovieModal = document.getElementById("delete-modal");
 
+// Buffer to hold the list of added movie objects
 const movies = [];
 
-const toggleBackdrop = () => {
-  backdrop.classList.toggle("visible");
-};
+// Variable to generate unique movie id
+let uniqueMovieID = 0;
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle("visible");
-  toggleBackdrop();
-};
-
-const clearInputs = () => {
-  for (inputField of userInputs) {
-    inputField.value = "";
+// Function to toggle the welcome display when buffer is empty
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
   }
 };
 
-const deleteMovie = (id) => {
+// Function to delete the movie
+// Gets triggered when the yes button in delete movie dialog is selected
+const deleteMovieHandler = (id) => {
   let index = 0;
-  for (movie of movies) {
+  for (const movie of movies) {
     if (movie.id === id) {
       break;
     }
     index++;
   }
   movies.splice(index, 1);
-
   const ul = document.getElementById("movie-list");
   ul.children[index].remove();
+  closeMovieDeletionModal();
+  updateUI();
 };
 
-const deleteMovieHandler = (id) => {
-  const deleteModal = document.getElementById("delete-modal");
-  deleteModal.style.display = "block";
-
-  const no = deleteModal.querySelector(".btn--passive");
-  const yes = deleteModal.querySelector(".btn--danger");
-
-  no.addEventListener("click", () => {
-    deleteModal.style.display = "none";
-  });
-  yes.addEventListener("click", () => {
-    deleteMovie(id);
-  });
-  yes.removeEventListener("click", () => {
-    deleteModal.style.display = "none";
-  });
-
-  // deleteMovie(id)
+// Function to cancel the movie deletion
+// Gets triggered when the no button in delete movie dialog is selected
+const closeMovieDeletionModal = () => {
+  toggleBackdrop();
+  deleteMovieModal.classList.remove("visible");
 };
 
-const renderMovies = (movie) => {
-  if (movies.length === 0) {
-    entryTextSection.style.display = "block";
-  } else {
-    entryTextSection.style.display = "none";
-  }
+// Function to start the process of movie deletion
+// Gets triggered when user clicks on a movie card
+// Opens a deletion confirmation dialog box to confirm or cancel the movie deletion
+const startDeleteMovieHandler = (id) => {
+  deleteMovieModal.classList.add("visible");
+  toggleBackdrop();
 
+  const cancelBtn = deleteMovieModal.querySelector(".btn--passive");
+  let confirmBtn = deleteMovieModal.querySelector(".btn--danger");
+
+  confirmBtn.replaceWith(confirmBtn.cloneNode(true));
+  confirmBtn = deleteMovieModal.querySelector(".btn--danger");
+
+  cancelBtn.removeEventListener("click", closeMovieDeletionModal);
+
+  cancelBtn.addEventListener("click", closeMovieDeletionModal);
+  confirmBtn.addEventListener("click", deleteMovieHandler.bind(null, id));
+};
+
+// Function to render a new movie element when added successfully
+// Newly added movie is seen as a card with relevant details
+const renderNewMovieElement = (movie) => {
   const newMovieElement = document.createElement("li");
   newMovieElement.className = "movie-element";
   newMovieElement.innerHTML = `
@@ -77,13 +81,44 @@ const renderMovies = (movie) => {
   `;
   newMovieElement.addEventListener(
     "click",
-    deleteMovieHandler.bind(null, movie.id)
+    startDeleteMovieHandler.bind(null, movie.id)
   );
-
   const ul = document.getElementById("movie-list");
   ul.appendChild(newMovieElement);
 };
 
+// Function to toggle the backdrop
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+// Function to close the add movie dialog
+const closeMovieModal = () => {
+  addMovieModal.classList.remove("visible");
+};
+
+// Function to show the add movie dialog
+const showMovieModal = () => {
+  addMovieModal.classList.add("visible");
+  toggleBackdrop();
+};
+
+// Function to clear the text fields in the add movie dialog
+const clearInputs = () => {
+  for (inputField of userInputs) {
+    inputField.value = "";
+  }
+};
+
+// Event handler for cancel button in add movie dialog
+const cancelAddMovieHandler = () => {
+  closeMovieModal();
+  clearInputs();
+  toggleBackdrop();
+};
+
+// Function to add a new movie to the database
+// Event handler for the add button in the add movie dialog
 const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
@@ -107,7 +142,7 @@ const addMovieHandler = () => {
   }
 
   movieObject = {
-    id: Math.random() * 100,
+    id: ++uniqueMovieID,
     title: titleValue.trim(),
     link: imageUrlValue.trim(),
     rating: ratingsNumber,
@@ -116,12 +151,25 @@ const addMovieHandler = () => {
   movies.push(movieObject);
   console.log(movieObject);
 
-  toggleMovieModal();
+  closeMovieModal();
+  toggleBackdrop();
   clearInputs();
-  renderMovies(movieObject);
+  renderNewMovieElement(movieObject);
+  updateUI();
+
+  // toggleMovieModal();
+  // clearInputs();
+  // renderMovies(movieObject);
 };
 
-addMovieButton.addEventListener("click", toggleMovieModal);
-backdrop.addEventListener("click", toggleMovieModal);
-modalCancelButton.addEventListener("click", toggleMovieModal);
+// Event handler for the backdrop DOM object
+const backdropClickHandler = () => {
+  closeMovieModal();
+  closeMovieDeletionModal();
+  clearInputs();
+};
+
+addMovieButton.addEventListener("click", showMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+modalCancelButton.addEventListener("click", cancelAddMovieHandler);
 modalAddButton.addEventListener("click", addMovieHandler);
